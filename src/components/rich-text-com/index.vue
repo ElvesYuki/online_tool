88415ce664e7085d0dea6eb8a93d334c20c94deb
ElvesYuki <1026770043@qq.com>
2025-01-15 14:36:12 +0800

    update .gitignore

--- src/components/rich-text-com/index.vue
+++ src/components/rich-text-com/index.vue
@@ -3,6 +3,17 @@
   <div class="editorBox">
     <div v-if="editor">
       <div v-if="editable" class="editorTop">
+        <div v-for="(item, index) in ['h1', 'h2', 'h3', 'h4']" :key="item">
+          <div
+            class="iconBtn"
+            :class="{
+              'is-active': editor.isActive('heading', { level: index + 1 }),
+            }"
+            @click="toggleHeading($event, index + 1)"
+          >
+            <component :is="'icon-' + item"></component>
+          </div>
+        </div>
         <div
           class="iconBtn"
           :class="{ 'is-active': editor.isActive('bold') }"
@@ -37,6 +48,54 @@
           @click="toggleAlignLeft"
         >
           <icon-align-left />
+        </div>
+        <div
+          class="iconBtn"
+          :class="{ 'is-active': editor.isActive({ textAlign: 'center' }) }"
+          @click="toggleAlignCenter"
+        >
+          <icon-align-center />
+        </div>
+        <div
+          class="iconBtn"
+          :class="{ 'is-active': editor.isActive({ textAlign: 'right' }) }"
+          @click="toggleAlignRight"
+        >
+          <icon-align-right />
+        </div>
+        <div
+          class="iconBtn"
+          :class="{ 'is-active': editor.isActive({ textAlign: 'bulletList' }) }"
+          @click="toggleBulletList"
+        >
+          <icon-unordered-list />
+        </div>
+        <div
+          class="iconBtn"
+          :class="{
+            'is-active': editor.isActive({ textAlign: 'orderedList' }),
+          }"
+          @click="toggleOrderedList"
+        >
+          <icon-ordered-list />
+        </div>
+        <div
+          class="iconBtn"
+          :class="{
+            'is-active': editor.can().undo(),
+          }"
+          @click="editor.chain().focus().undo().run()"
+        >
+          <icon-undo />
+        </div>
+        <div
+          class="iconBtn"
+          :class="{
+            'is-active': editor.can().redo(),
+          }"
+          @click="editor.chain().focus().redo().run()"
+        >
+          <icon-redo />
         </div>
         <bubble-menu
           class="bubble-menu"
@@ -80,6 +139,95 @@
           </div>
         </bubble-menu>
       </div>
+      <div v-if="editable" class="editorTableTop">
+        <div class="iconTableBtn">
+          <span style="font-size: 14px">表格工具：</span>
+        </div>
+        <div
+          class="iconTableBtn"
+          @click="
+            editor
+              .chain()
+              .focus()
+              .insertTable({ rows: 3, cols: 3, withHeaderRow: true })
+              .run()
+          "
+        >
+          新增表格
+        </div>
+        <div
+          class="iconTableBtn"
+          @click="editor.chain().focus().addColumnBefore().run()"
+        >
+          向前新增一列
+        </div>
+        <div
+          class="iconTableBtn"
+          @click="editor.chain().focus().addColumnAfter().run()"
+        >
+          向后新增一列
+        </div>
+        <div
+          class="iconTableBtn"
+          @click="editor.chain().focus().deleteColumn().run()"
+        >
+          删除当前列
+        </div>
+        <div
+          class="iconTableBtn"
+          @click="editor.chain().focus().addRowBefore().run()"
+        >
+          向前新增一行
+        </div>
+        <div
+          class="iconTableBtn"
+          @click="editor.chain().focus().addRowAfter().run()"
+        >
+          向后新增一行
+        </div>
+        <div
+          class="iconTableBtn"
+          @click="editor.chain().focus().deleteRow().run()"
+        >
+          删除当前行
+        </div>
+        <div
+          class="iconTableBtn"
+          @click="editor.chain().focus().deleteTable().run()"
+        >
+          删除表格
+        </div>
+        <div
+          class="iconTableBtn"
+          @click="editor.chain().focus().mergeOrSplit().run()"
+        >
+          拆分或合并
+        </div>
+        <div
+          class="iconTableBtn"
+          @click="editor.chain().focus().toggleHeaderCell().run()"
+        >
+          设置背景色
+        </div>
+        <div
+          class="iconTableBtn"
+          @click="editor.chain().focus().fixTables().run()"
+        >
+          修复单元格
+        </div>
+        <div
+          class="iconTableBtn"
+          @click="editor.chain().focus().goToNextCell().run()"
+        >
+          下一格
+        </div>
+        <div
+          class="iconTableBtn"
+          @click="editor.chain().focus().goToPreviousCell().run()"
+        >
+          上一格
+        </div>
+      </div>
     </div>
     <editor-content
       :editor="editor"
@@ -93,6 +241,7 @@
   import { ref, onUnmounted, computed } from 'vue';
   import { EditorContent, BubbleMenu, useEditor } from '@tiptap/vue-3';
   import StarterKit from '@tiptap/starter-kit';
+  import { Level } from '@tiptap/extension-heading';
   // import { Placeholder } from '@tiptap/extension-placeholder';
   import Underline from '@tiptap/extension-underline';
   import { Image } from '@tiptap/extension-image';
@@ -100,6 +249,10 @@
   import TextAlign from '@tiptap/extension-text-align';
   import TextStyle from '@tiptap/extension-text-style';
   import CharacterCount from '@tiptap/extension-character-count';
+  import { Table } from '@tiptap/extension-table';
+  import { TableRow } from '@tiptap/extension-table-row';
+  import { TableHeader } from '@tiptap/extension-table-header';
+  import { TableCell } from '@tiptap/extension-table-cell';
   // import ListItem from '@tiptap/extension-list-item';
 
   // 创建编辑器实例
@@ -119,6 +272,12 @@
         types: ['heading', 'paragraph'],
       }),
       CharacterCount.configure({}),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
     ],
     content: {
       type: 'doc',
@@ -150,6 +309,16 @@
       }
     },
   });
+
+  // 点击标题
+  const toggleHeading = (event: MouseEvent, level: number) => {
+    event.preventDefault(); // 防止按钮默认行为
+    editor.value
+      ?.chain()
+      .focus()
+      .toggleHeading({ level: level as Level })
+      .run();
+  };
 
   // 点击加粗
   const toggleBold = (event: MouseEvent) => {
@@ -272,14 +441,15 @@
 
 <style scoped lang="less">
   .editorBox {
+    box-sizing: border-box;
     position: relative;
     border: 1px solid #ccc;
     border-radius: 8px;
     overflow: hidden;
+    background: #ffffff;
   }
   .editorTop {
-    box-sizing: border-box;
-    padding: 0 20px;
+    padding: 0 10px;
     width: 100%;
     height: 40px;
     top: 0;
@@ -288,24 +458,72 @@
     background: #fff;
     display: flex;
     align-items: center;
+    gap: 2px;
     .inputColor {
-      width: 20px;
-      height: 20px;
+      width: 30px;
+      height: 30px;
       cursor: pointer;
     }
     .iconBtn {
-      width: 22px;
-      height: 22px;
+      width: 32px;
+      height: 32px;
       box-sizing: content-box;
       padding: 1px;
       cursor: pointer;
-      margin-left: 4px;
       display: flex;
       justify-content: center;
       align-items: center;
       img {
         max-width: 100%;
         max-height: 100%;
+      }
+      .arco-icon {
+        width: 1.5em;
+        height: 1.5em;
+      }
+      &:hover {
+        background: #ebebeb;
+        border-radius: 6px;
+      }
+    }
+    .is-active {
+      background: #ebebeb;
+      border-radius: 6px;
+    }
+  }
+  .editorTableTop {
+    padding: 0 10px;
+    width: 100%;
+    height: 50px;
+    top: 0;
+    left: 0;
+    border-bottom: 1px solid #ccc;
+    background: #fff;
+    display: flex;
+    align-items: center;
+    gap: 2px;
+    .inputColor {
+      width: 30px;
+      height: 30px;
+      cursor: pointer;
+    }
+    .iconTableBtn {
+      width: 72px;
+      height: 44px;
+      box-sizing: content-box;
+      padding: 1px;
+      cursor: pointer;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 12px;
+      img {
+        max-width: 100%;
+        max-height: 100%;
+      }
+      .arco-icon {
+        width: 1.5em;
+        height: 1.5em;
       }
       &:hover {
         background: #ebebeb;
